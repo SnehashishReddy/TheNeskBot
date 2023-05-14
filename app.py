7185dbe7ae9e32e5407a9b493db189eb1597c0b1
Snehashish Reddy <ashishreddy9000@gmail.com>
2023-05-14 15:58:59 +0530

    feat: Add summarizer for the bot

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-from fable.credentials import bot_token, bot_username
+from fable.credentials import bot_token
+from fable.ocr.char_rec import character_recognise
+from fable.summarizer.summarizer import summarize
 
 import logging
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
+from PIL import Image
+import io
+
 
 introduction = 'Welcome to interaction with F.A.B.L.E, please read the description and feel free to ask the bot a question!'
 
@@ -17,17 +22,37 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 async def message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text = update.message.text
-    print(text)
     
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I am currently in production, please check back later.")
+    summarized_text = summarize(text)
+    
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=summarized_text)
+
+async def image(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    img_file = await context.bot.get_file(update.message.document)
+    
+    image_bytes = io.BytesIO()
+    
+    await img_file.download_to_memory(out=image_bytes)
+    
+    image_bytes.seek(0)
+    
+    img = Image.open(image_bytes).convert('L')
+    
+    text = character_recognise(img)
+    
+    summarized_text = summarize(text)
+    
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=summarized_text)
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(bot_token).build()
     
     start_handler = CommandHandler('start', start)
-    message_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), message)
+    message_handler = MessageHandler(filters.TEXT, message)
+    image_handler = MessageHandler(filters.Document.IMAGE, image)
     
     application.add_handler(start_handler)
     application.add_handler(message_handler)
+    application.add_handler(image_handler)
     
     application.run_polling()
---
+++ fable/summarizer/summarizer.py
@@ -0,0 +1,23 @@
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lex_rank import LexRankSummarizer
+from sumy.nlp.stemmers import Stemmer
+
+def summarize(text_article: str) -> str:
+    return LexRankSummary(text_article)
+
+def LexRankSummary(text_article: str) -> str:
+    parser = PlaintextParser.from_string(text_article, Tokenizer("english"))
+    summarizer = LexRankSummarizer(stemmer=Stemmer('english'))
+    
+    try:
+        summary = summarizer(parser.document, 3)
+
+        answer = []
+
+        for sentence in summary:
+            answer.append(sentence.__str__())
+
+        return(' '.join(answer))
+    except RuntimeError:
+        return "The given text could not be summarized."
